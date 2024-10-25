b94601df6e506835005506ab559fee3f8c39dfc7
nenava97 <108698688+nenava97@users.noreply.github.com>
2024-10-25 08:48:52 -0400

    Update ml_frontend_server.sh
    -y

--- terraform/ml_frontend_server.sh
+++ terraform/ml_frontend_server.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
-sudo apt-get update
+# Automatically respond "yes" to prompts
+sudo apt-get update -y
 
-sudo apt install python3-pip python3-venv
+sudo apt install -y python3-pip python3-venv
 
-
-# download repo
+# Download repo
 git clone https://github.com/elmorenox/CNN_deploy.git
 
-cd ~/CNN_DEPLOY/pneumonia_web
+cd ~/CNN_deploy/pneumonia_web
 
+# Set up virtual environment
 python3 -m venv venv
 
-source venv/bin/active
+source venv/bin/activate
 
+# Install required packages
 pip install --upgrade pip
 pip install -r requirements.txt
 
+# Start the application
 gunicorn --config gunicorn_config.py app:app
